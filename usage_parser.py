498684344e54d3eef9349c53312efb2c8c4848b0
Amara Obasi <aobasi4@gmail.com>
2023-06-17 10:25:07 -0600

    All tests passing

--- usage_parser.py
+++ usage_parser.py
@@ -28,15 +28,21 @@ class UsageParser(object):
         basicList = {}
 
         basicList["id"] = int(obj[0])
-        basicList["dmcc"] = int(obj[1][0:4])
-        basicList["mnc"] = None
-        basicList["bytes_used"] = None
-        basicList["cellid"] = None
-        basicList["ip"] = None
+        basicList["mnc"] = int(obj[1][0:4],16)
+        basicList["dmcc"] = None
+        basicList["bytes_used"] = int(obj[1][4:8],16)
+        basicList["cellid"] = int(obj[1][8:16],16)
+        basicList["ip"] = UsageParser.createHexIP(obj[1][16:])
 
         return (basicList)
         
         pass
+    def createHexIP (obj) -> typing.ByteString:
+        bytes = ["".join(x) for x in zip(*[iter(obj)]*2)]
+        bytes = [int(x, 16) for x in bytes]
+        ip = ".".join(str(x) for x in bytes)
+        return ip
+
     #Takes in a List and return a JSON-ish object containing the ID and bytes_used which are both integers
     # Note to Amara: You can speicfy that this List is String to Int for clairty 
     # Note: The parse method essentially will be a for loop that creates the Json object because you have multiple per input! 

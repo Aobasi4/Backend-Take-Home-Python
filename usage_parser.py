7b6c0438b1c46d7eb7fab4fc1d3fded1b8ecbe5c
Amara Obasi <aobasi4@gmail.com>
2023-06-16 22:39:12 -0600

    Fixed parseBasic method + added comments for next steps in workflow

--- usage_parser.py
+++ usage_parser.py
@@ -3,22 +3,37 @@ import typing
 
 class UsageParser(object):
     @staticmethod
-    def parse(*input: str) -> typing.List[typing.Mapping[str, typing.Any]]:
-        #This method will determine which of the other parse methods should be used and then pass the
-        # Object to said parse method. Then return it
-        pass
     def parseExtended (object) -> typing.List:
         pass
+    @staticmethod
     def parseHex (object) -> typing.List:
         pass
     #Takes in a List and return a JSON-ish object containing the ID and bytes_used which are both integers
     # Note to Amara: You can speicfy that this List is String to Int for clairty 
-    def parseBasic (object) -> typing.List:
+    # Note: The parse method essentially will be a for loop that creates the Json object because you have multiple per input! 
+    # Make variable names more conscise and make sure you're workflow is well commented and stuff
+    @staticmethod
+    def parseBasic (obj) -> typing.List:
         basicList = {}
 
-        basicList["id"] = object[0]
-        basicList["bytes_used"] = object[1]
+        basicList["id"] = obj[0]
+        basicList["bytes_used"] = obj[1]
 
-        return ([basicList])
-        pass
+        return (basicList)
+        
+
+    @staticmethod
+    def parse(*input: str) -> typing.List[typing.Mapping[str, typing.Any]]:
+        #This method will determine which of the other parse methods should be used and then pass the
+        # Object to said parse method. Then return it
+        finalEmptyList = []
+        decodeArray = input[0].split(",")
+        id = decodeArray[0]
+        #Determine what kind of parsing needs to be done
+        if id[-1] == 4:
+           finalEmptyList.append(UsageParser.parseExtended(decodeArray))
+        elif id[-1] == 6:
+            finalEmptyList.append(UsageParser.parseHex(decodeArray))
+        else:
+            finalEmptyList.append(UsageParser.parseBaisc(decodeArray))
 
